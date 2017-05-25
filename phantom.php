af6368789b671df5b615cbe05dab416a529fe6f6
Jeroen van der Steen <jeo_recordz@hotmail.com>
2017-05-25 19:19:24 +0200

    Adjusted composer for Phantomjs, and setup.sh

--- phantom.php
+++ phantom.php
@@ -1,14 +1,23 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require 'vendor/autoload.php';
+
 use JonnyW\PhantomJs\Client;
 
 $client = Client::getInstance();
-$client->getEngine()->setPath('/usr/bin/phantomjs');
+//$client->getEngine()->setPath('/usr/bin/phantomjs');
+$client->getEngine()->setPath('/opt/phantomjs');
+
+var_dump($client);
 
 /** 
  * @see JonnyW\PhantomJs\Http\Request
  **/
-$request = $client->getMessageFactory()->createRequest('http://jonnyw.me', 'GET');
+$request = $client->getMessageFactory()->createRequest('http://jeroensteen.nl', 'GET');
+$request->setTimeout(10000);
 
 /** 
  * @see JonnyW\PhantomJs\Http\Response 
@@ -17,6 +26,9 @@ $response = $client->getMessageFactory()->createResponse();
 
 // Send the request
 $client->send($request, $response);
+
+var_dump($response);
+var_dump($request);
 
 if($response->getStatus() === 200) {
 
--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
 #Setup of Terma, chmod a+x setup.sh
 mkdir frogs wikis
+
+#Give user:group permissions to all files
+#Give read/write permissions to frogs wikis
+#Give read/write permissions to /stopwords/all.txt
