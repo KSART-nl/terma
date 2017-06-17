583d85edfadf1c7f329e0c64d208c6f6a665186b
Jeroen van der Steen <info@jeroensteen.nl>
2017-06-17 15:11:56 +0200

    Changed css selector

--- selenium.php
+++ selenium.php
@@ -5,9 +5,10 @@ namespace Facebook\WebDriver;
 function get_woordenlijst_table($term, $driver) {
 	$target_url = "http://woordenlijst.org/#/?q=".$term;
 	$driver->get($target_url);
+	$driver->wait(0, 10);
 	$driver->manage()->timeouts()->implicitlyWait = 10;
 	try {
-		$element = $driver->findElement(WebDriverBy::cssSelector('table.pos-listing-table'));
+		$element = $driver->findElement(WebDriverBy::cssSelector('table.info-table.pos-listing-table tr td span'));
 		var_dump($element);
 	} catch (Exception\NoSuchElementException $e) {
 		echo "Element not found exception for: ".$target_url."\n";
