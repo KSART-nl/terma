ca5afce1db6a5a3f0bb340dfbe8172ae5ebf31ec
Jeroen van der Steen <info@jeroensteen.nl>
2017-06-19 10:01:37 +0200

    Changed selenium code

--- selenium.php
+++ selenium.php
@@ -6,7 +6,7 @@ function get_woordenlijst_table($term, $driver) {
 	$target_url = "http://woordenlijst.org/#/?q=".$term;
 	$driver->get($target_url);
 	$driver->wait(0, 10);
-	$driver->manage()->timeouts()->implicitlyWait = 10;
+	$driver->manage()->timeouts()->implicitlyWait(10);
 	try {
 		$elements = $driver->findElement(WebDriverBy::cssSelector('table.info-table.pos-listing-table tr td span'));
 		
