203eeaab81051015e0b6a4158383006870b5f6d6
Jeroen van der Steen <info@jeroensteen.nl>
2017-06-20 12:07:16 +0200

    Changed index

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $neo4j = ClientBuilder::create()
     ->build();
 
 // Result contains a collection (array) of Record objects
-$result = $neo4j->run(
+$result = @$neo4j->run(
 'MATCH (s:ns0_ScopeNote)<-[rel:ns1_scopeNote]-(r:Resource)
 WHERE s.ns6_value IS NOT NULL AND r.ns2_label IS NOT NULL
 RETURN
@@ -51,9 +51,7 @@ r.ns2_label AS label,
 r.ns1_prefLabel AS prefLabel,
 r.ns1_altLabel AS altLabel,
 r.ns0_parentString AS parentString,
-
 s.ns6_value AS scopeNote
-
 LIMIT 5'
 );
 // Get all records
